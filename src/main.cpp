40fdd08093780967cf0ce20036ea94caa6e67b97
Parth Patel <parthp.tech@gmail.com>
2026-09-07 09:42:43 +0530

    Update main.cpp to demonstrate the library

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 char ssid[] = "PARTH";
 char pass[] = "iamparth";
 
-std::string server = "http://10.122.132.89:5000/api/v1/check-update?current_version=1.0";
-std::string checksum = "http://10.197.157.89:8000/firmware.crc32";
-std::string checksum_update = "http://10.197.157.89:8000/firmware";
-std::string api = "5556273209c07bc50627abfe9fd914d0ee4edf7743ce2f50451f91007fcce3b8";
+std::string firmware = "http://10.0.107.68:5000/api/v1/download-firmware";
+std::string checksum = "http://10.0.107.68:5000/api/v1/checksum";
+std::string check_update = "http://10.0.107.68:5000/api/v1/check-update?current_version=1";
+std::string api = "5634e88e05ea12e4f475f2b71542db988031cc6b49541cc982743521dd3b73f2";
 
 #define CHECK_PIN 21
 #define START_PIN 22
 
-OTAUpdater updater(server, checksum, checksum_update);
+OTAUpdater updater(firmware, checksum, check_update, api);
 
 void setup()
 {
@@ -42,23 +42,45 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(CHECK_PIN) == LOW)
+  OTAResult result = updater.check_for_update();
+
+  switch (result)
   {
-    OTAResult result = updater.check_for_update();
-
-    switch (result)
+  case OTAResult::HTTP_ERROR:
+    Serial.println("HTTP Error");
+    break;
+  case OTAResult::OK:
+  {
+    Serial.println("Update Available");
+    OTAResult update = updater.download_firmware();
+    switch (update)
     {
-    case OTAResult::HTTP_ERROR:
-      Serial.println("HTTP Error");
-      break;
-
     case OTAResult::OK:
-      Serial.println("Update Available");
-      break;
+      Serial.println("Ok");
+      OTAResult change_bootorder_result = updater.change_bootorder();
 
+      if (change_bootorder_result != OTAResult::OK)
+      {
+        Serial.println("Unable to change bootorder");
+        break;
+      }
+
+      Serial.println("Bootorder changed successfully");
+      break;
     case OTAResult::NO_UPDATE_AVAILABLE:
       Serial.println("Update unavailable");
       break;
+    case OTAResult::INVALID_API_KEY:
+      Serial.println("Invalid API key");
+    default:
+      Serial.println("Failed to start");
+      break;
     }
+    break;
   }
+  default:
+    break;
+  }
+
+  delay(5000);
 }
